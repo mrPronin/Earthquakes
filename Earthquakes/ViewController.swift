1822f6cfee7575314695fa4910eeaa31fbb7de6d
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2020-02-07 15:09:41 +0100

    Initial Commit

---
+++ Earthquakes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Earthquakes
+//
+//  Created by Oleksandr Pronin on 07.02.20.
+//  Copyright © 2020 pronin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
