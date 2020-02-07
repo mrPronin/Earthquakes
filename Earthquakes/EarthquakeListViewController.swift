480a3d54b1a2265385fa0f34428febe8fdb6e132
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2020-02-07 15:29:08 +0100

    Initial commit

--- Earthquakes/EarthquakeListViewController.swift
+++ Earthquakes/EarthquakeListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  EarthquakeListViewController.swift
 //  Earthquakes
 //
 //  Created by Oleksandr Pronin on 07.02.20.
@@ -8,13 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class EarthquakeListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.2'
+
+target 'Earthquakes' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Earthquakes
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'SwiftLint'
+end
